4af942bf574b4844b56ebc8dc8643733f449862f
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-08-24 17:48:20 +0500

    update dll header to create serial port connection

--- dll/include/serial.h
+++ dll/include/serial.h
@@ -3,18 +3,24 @@
 class CSerial
 {
 private:
-    HANDLE	m_hThread;
-
+    HANDLE	m_hPort;                //port handle
+    HANDLE	m_hThread;              //thread handle
+    bool	m_fReading;             //reading cycle exit flag
+    void    (*m_pCallback)(char*);     //callback method to send read bytes
 public:
-    void (*m_pCallback)(char*);
     CSerial();
     ~CSerial();
+    bool Open(int nPortNumber, int nBaudRate); //open port with nPortNumber and
     void read(void (*callback)(char*));
-    static DWORD WINAPI thread_func(LPVOID lpParam);
+private:
+    bool OpenPort();
+    bool SetupConnection();
+    bool StartReading();
+    static DWORD WINAPI thread_func(LPVOID lpParam);  //read method in other thread
 };
 
 extern "C"{
-#define EXPORT __declspec(dllexport)
+    #define EXPORT __declspec(dllexport)
     EXPORT CSerial* __stdcall newCSerial();
     EXPORT void __stdcall delCSerial(CSerial* pSerial);
     EXPORT void __stdcall serialRead(CSerial* pSerial, void (*callback)(char*));
