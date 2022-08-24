6022758f908aae5db2580eef385db536b631343f
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-08-24 15:45:48 +0500

    create inclass method with callback

--- dll/include/serial.h
+++ dll/include/serial.h
@@ -4,6 +4,7 @@ public:
     CSerial();
     ~CSerial();
     char add(char a, char b);
+    void read(void (*callback)(char*));
 };
 
 extern "C"{
@@ -11,5 +12,5 @@ extern "C"{
     EXPORT CSerial* __stdcall newCSerial();
     EXPORT void __stdcall delCSerial(CSerial* pSerial);
     EXPORT char __stdcall serialAdd(CSerial* pSerial, char a, char b);
+    EXPORT void __stdcall serialRead(CSerial* pSerial, void (*callback)(char*));
 }
-
--- dll/src/serial.cpp
+++ dll/src/serial.cpp
@@ -1,4 +1,6 @@
+#include <cstddef>
 #include "serial.h"
+
 
 CSerial::CSerial()
 {
@@ -15,6 +17,14 @@ char CSerial::add(char a, char b)
     return a + b;
 }
 
+void CSerial::read(void (*callback)(char*))
+{
+    if (NULL != callback) {
+        char datas[]={0x43,0x4d,0x30,0x30,0x0f,0x0D};
+        callback(datas);
+    }
+}
+
 CSerial* __stdcall newCSerial()
 {
     return new CSerial();
@@ -22,9 +32,15 @@ CSerial* __stdcall newCSerial()
 
 void __stdcall delCSerial(CSerial* pSerial)
 {
-    pSerial->~CSerial();
+    delete pSerial;
 }
+
 char __stdcall serialAdd(CSerial* pSerial, char a, char b)
 {
     return pSerial->add(a, b);
+}
+
+void __stdcall serialRead(CSerial* pSerial, void (*callback)(char*))
+{
+    pSerial->read(callback);
 }
