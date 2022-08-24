e0c9e3f0647aacc2b1a7be68949922fc5cccab24
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-08-24 18:14:52 +0500

    update header

--- dll/include/serial.h
+++ dll/include/serial.h
@@ -11,18 +11,18 @@ public:
     CSerial();
     ~CSerial();
     bool    Open(int port_number, int baud_rate, void (*callback)(char*));  //open port with port_number and baud_rate
+    bool    Connected();                                                    //return status of connection
     void    Close();                                                        //close port
     int     Write(char* data, int size);                                    //write bytes to port
 private:
     bool    CreateConnection();                                             //create connection to port
     bool    SetupConnection();                                              //setup timeouts and [pr
     bool    StartReading();                                                 //start reading thread
-    static DWORD WINAPI thread_func(LPVOID lpParam);                        //read method in other thread
+    static DWORD WINAPI Read(LPVOID lpParam);                        //read method in other thread
 };
 
 extern "C"{
     #define EXPORT __declspec(dllexport)
     EXPORT CSerial* __stdcall newCSerial();
     EXPORT void __stdcall delCSerial(CSerial* pSerial);
-    EXPORT void __stdcall serialRead(CSerial* pSerial, void (*callback)(char*));
 }
