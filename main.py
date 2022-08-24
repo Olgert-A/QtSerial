7c05d3af3f6e196ec39d77cd024717c9eb1daa57
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-08-24 15:49:26 +0500

    create pyqt simple form
    callback method will show data in QList

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-import ctypes
-from ctypes import cdll
+import sys
+from PyQt6.QtWidgets import QApplication
+from view import MainView
 
-
-class Serial(object):
-    def __init__(self):
-        self.lib = cdll.LoadLibrary('.\\dll\\serial.dll')
-        self.lib.newCSerial.argtypes = []
-        self.lib.newCSerial.restype = ctypes.c_void_p
-
-        self.lib.serialAdd.argtypes = [ctypes.c_void_p, ctypes.c_char, ctypes.c_char]
-        self.lib.serialAdd.restype = ctypes.c_char
-
-        self.obj = self.lib.newCSerial()
-
-    def add(self, a, b):
-        return self.lib.serialAdd(self.obj, a, b)
-
-    def __del__(self):
-        self.lib.delCSerial()
-
-
-serial = Serial()
-val = serial.add(251, 5)
-print(val)
-
+app = QApplication([])
+view = MainView()
+view.show()
+sys.exit(app.exec())
---
+++ view.py
@@ -0,0 +1,29 @@
+import ctypes
+from PyQt6.QtWidgets import QMainWindow, QListWidget, QPushButton, QVBoxLayout, QWidget
+from dll.pyserial import Serial
+import ctypes
+
+
+class MainView(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.read_list = QListWidget()
+        self.open_port = QPushButton('Open port')
+
+        self.open_port.pressed.connect(self.open_port_pressed)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.open_port)
+        layout.addWidget(self.read_list)
+
+        main_widget = QWidget()
+        main_widget.setLayout(layout)
+        self.setCentralWidget(main_widget)
+
+    def open_port_pressed(self):
+        pass
+
+    def read_event(self, data):
+        formatted_data = ' '.join([f'{byte:02X}' for byte in data])
+        self.read_list.addItem(f'Received {len(data)}: {formatted_data}')
