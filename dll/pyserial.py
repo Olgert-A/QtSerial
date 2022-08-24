650d5b119d61846722fa01ff7c4820389dc923bc
Oleg <106367421+Olgert-A@users.noreply.github.com>
2022-08-24 19:20:49 +0500

    update python serial class with new dll structure

--- dll/pyserial.py
+++ dll/pyserial.py
@@ -1,24 +1,40 @@
 import ctypes
-from ctypes import cdll, CFUNCTYPE
+from ctypes import cdll, c_void_p, c_int, c_char_p, c_bool
 
 
 class Serial(object):
     def __init__(self):
         self.lib = cdll.LoadLibrary('.\\dll\\serial.dll')
-        self.lib.newCSerial.argtypes = []
-        self.lib.newCSerial.restype = ctypes.c_void_p
+        self.init_methods()
+        self.serial_object = self.lib.CreateSerial()
 
-        self.lib.delCSerial.argtypes = [ctypes.c_void_p]
-        self.lib.delCSerial.restype = None
+    def init_methods(self):
+        self.lib.CreateSerial.argtypes = []
+        self.lib.DeleteSerial.argtypes = [c_void_p]
+        self.lib.SerialOpen.argtypes = [c_void_p, c_int, c_int, c_void_p]
+        self.lib.SerialConnected.argtypes = [c_void_p]
+        self.lib.SerialClose.argtypes = [c_void_p]
+        self.lib.SerialWrite.argtypes = [c_void_p, c_char_p, c_int]
 
-        self.lib.serialRead.argtypes = [ctypes.c_void_p, ctypes.c_void_p]
-        self.lib.serialRead.restype = None
+        self.lib.CreateSerial.restype = ctypes.c_void_p
+        self.lib.DeleteSerial.restype = None
+        self.lib.SerialOpen.restype = c_bool
+        self.lib.SerialConnected.restype = c_bool
+        self.lib.SerialClose.restype = None
+        self.lib.SerialWrite.restype = c_int
 
-        self.obj = self.lib.newCSerial()
+    def open(self, port_number, baud_rate, callback) -> bool:
+        return self.lib.SerialOpen(self.serial_object, port_number, baud_rate, callback)
 
-    def read(self, callback):
-        self.lib.serialRead(self.obj, callback)
+    def connected(self) -> bool:
+        return self.lib.SerialConnected(self.serial_object)
+
+    def close(self) -> None:
+        self.lib.SerialClose(self.serial_object)
+
+    def write(self, data: bytes) -> int:
+        return self.lib.SerialWrite(self.serial_object, data, len(data))
 
     def __del__(self):
-        if self.lib and self.obj:
-            self.lib.delCSerial(self.obj)
+        if self.lib and self.serial_object:
+            self.lib.DeleteSerial(self.serial_object)
