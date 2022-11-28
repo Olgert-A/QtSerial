32da1a9dbd60621b5aef3447d3f8b25c66e76e46
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2022-11-28 16:34:07 +0500

    create data collecting code and show graph after

--- data.py
+++ data.py
@@ -3,49 +3,77 @@ from dll.pyserial import Serial
 
 
 class SerialData:
-    def __init__(self):
+    def __init__(self, view_callback):
+        self.read_buffer = []
+        self.collecting = False  # show that cycled measurement collecting was started
+        self.measurements = []
+        self.serial = Serial()
+        self.view_callback = view_callback
         # init callback functype and set
         cb_type = ctypes.CFUNCTYPE(None, ctypes.c_char_p)
         self.callback = cb_type(self.read_event)
-        self.serial = Serial()
-        opened = self.serial.open(4, 115200, self.callback)
-        self.data = []
-        print(opened)
+        # init serial
+        opened = self.serial.open(2, 115200, self.callback)
 
     def __del__(self):
         if self.serial:
             self.serial.close()
 
     def read_event(self, data):
-        self.data.append(data)
-        print(self.data)
-        self.check_packet()
+        if data == b'':
+            data = b'\x00'
+        self.view_callback(data)
+        self.read_buffer.append(data)
+
+        if self.collecting:
+            if self.is_meas_packet():
+                self.measurements.append(self.get_meas())
+                self.read_buffer.clear()
+                self.send_meas_request()
 
     def write(self, data: bytes):
         if data:
             self.serial.write(data)
 
-    def check_packet(self):
-        if len(self.data) >= 6:
-            packet = self.data[:6]
-            print(packet)
-            cmd_to_check = self.mask_packet(packet, [255, 255, 0, 0, 255, 0])
-            if [2, 6, 0, 0, 3, 0] != cmd_to_check or not self.check_xor(packet):
-                self.data = self.data[1:]
-            else:
-                data = b''.join(packet[2:4])
-                data = int.from_bytes(data, byteorder='big')
-                self.data = self.data[6:]
-                print(data)
+    def start_collecting(self):
+        self.read_buffer.clear()
+        self.measurements.clear()
+        self.collecting = True
+        self.send_meas_request()
 
+    def stop_collecting(self):
+        self.collecting = False
 
-    def mask_packet(self, packet, mask):
-        packet = b''.join(packet)
-        mask = bytes(mask)
-        return [a & b for a, b in zip(packet, mask)]
+    def get_collected(self):
+        return self.measurements
 
-    def check_xor(self, data):
+    def send_meas_request(self):
+        packet = '02 43 B0 01 03 F2'
+        bts = [int(b, 16) for b in packet.split()]
+        self.serial.write(bytes(bts))
+
+    def is_meas_packet(self):
+        if len(self.read_buffer) == 6:
+            if self.check_meas_cmd() and self.check_xor():
+                return True
+        return False
+
+    def check_meas_cmd(self):
+        data = self.read_buffer[:6]
+        return ord(data[0]) == 2 and ord(data[1]) == 6 and ord(data[4]) == 3
+
+    def check_xor(self):
+        data = self.read_buffer[:6]
         return ord(data[1]) ^ ord(data[2]) ^ ord(data[3]) == ord(data[5])
 
+    def get_meas(self):
+        packet = self.read_buffer[:6]
+        data_bytes = b''.join(packet[2:4])
+        meas = int.from_bytes(data_bytes, byteorder='big')
+        return self.convert_to_len(meas)
 
-
+    def convert_to_len(self, meas):
+        if meas <= int('0x1388', 16):
+            return meas
+        if meas >= int('0xEC78', 16):
+            return meas - int('0xFFFF', 16) - 1
--- view.py
+++ view.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QMainWindow, QListWidget, QPushButton, QVBoxLayout, QWidget, QLineEdit
+from PyQt6.QtWidgets import QMainWindow, QListWidget, QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QLineEdit, QPlainTextEdit
+import pyqtgraph as pg
 from data import SerialData
 
 
@@ -6,30 +7,61 @@ class MainView(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.serial = SerialData()
+        self.serial = SerialData(self.read_event)
 
         self.read_list = QListWidget()
         self.write_data = QLineEdit()
         self.write_btn = QPushButton('Write')
 
-        self.write_btn.pressed.connect(self.write_btn_pressed)
+        self.meas_btn = QPushButton('Start meas')
+        self.graph = view = pg.PlotWidget()
+        self.curve = view.plot(name="Line")
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.read_list)
-        layout.addWidget(self.write_data)
-        layout.addWidget(self.write_btn)
+        self.write_btn.pressed.connect(self.write_btn_pressed)
+        self.meas_btn.pressed.connect(self.meas_btn_pressed)
+
+        row = QHBoxLayout()
+
+        col1 = QVBoxLayout()
+        col1.addWidget(self.read_list)
+        col1.addWidget(self.write_data)
+        col1.addWidget(self.write_btn)
+
+        col2 = QVBoxLayout()
+        col2.addWidget(self.meas_btn)
+        col2.addWidget(self.graph)
+
+        row.addLayout(col1)
+        row.addLayout(col2)
 
         main_widget = QWidget()
-        main_widget.setLayout(layout)
+        main_widget.setLayout(row)
         self.setCentralWidget(main_widget)
 
     def read_event(self, data):
         formatted_data = ' '.join([f'{byte:02X}' for byte in data])
-        self.read_list.addItem(f'Received {len(data)}: {formatted_data}')
+        self.read_list.addItem(formatted_data)
+        #self.read_list.addItem(f'Received {len(data)}: {formatted_data}')
 
     def write_btn_pressed(self):
-        packet = '02 43 B0 01 03 F2'
+        packet = self.write_data.text()
+        #packet = '02 43 B0 01 03 F2'
         bts = [int(b, 16) for b in packet.split()]
         self.serial.write(bytes(bts))
 
-
+    def meas_btn_pressed(self):
+        # print(int('0x00', 16))
+        # print(bytes([0]))
+        if not self.serial.collecting:
+            print('Start')
+            self.meas_btn.setText("Stop")
+            self.write_btn.setEnabled(False)
+            self.serial.start_collecting()
+        else:
+            print('Stop')
+            self.serial.stop_collecting()
+            data = self.serial.get_collected()
+            print(data)
+            self.curve.setData(data)
+            self.meas_btn.setText("Start")
+            self.write_btn.setEnabled(True)
